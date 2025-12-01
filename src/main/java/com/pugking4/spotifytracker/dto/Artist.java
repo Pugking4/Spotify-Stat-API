31a6bf9a59f55ab28ac4b63f45b6d57cf5f2fc9b
pugking4 <pugking4@protonmail.com>
2025-12-02 05:41:50 +1100

    Added new artist def and fixed code for it, also added most niche and popular artist stats.

--- src/main/java/com/pugking4/spotifytracker/dto/Artist.java
+++ src/main/java/com/pugking4/spotifytracker/dto/Artist.java
@@ -1,3 +1,6 @@
 package com.[REDACTED].spotifytracker.dto;
 
-public record Artist(String id, String name) {}
+import java.time.Instant;
+import java.util.List;
+
+public record Artist(String id, String name, Integer followers, List<String> genres, String image, Integer popularity, Instant updatedAt) {}
